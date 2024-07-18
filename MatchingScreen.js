98dc028ab918d3a891aa52adf6f20b5422b66b82
parkerg16 <parker.goins2016@gmail.com>
2024-07-18 19:50:24 -0400

    modified matching screen to add matches as well as chats for matches. modified matching screen to use onSnapshot listener instead of a fetch method.

--- MatchingScreen.js
+++ MatchingScreen.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert, InteractionManager } from 'react-native';
 import { auth, firestore } from './firebaseConfig';
-import { collection, getDocs, doc, getDoc, setDoc } from 'firebase/firestore';
+import { collection, getDocs, doc, getDoc, updateDoc, arrayUnion } from 'firebase/firestore';
+import { v4 as uuidv4 } from 'uuid';
+import 'react-native-get-random-values';
 
 const MatchingScreen = ({ navigation }) => {
   const [matches, setMatches] = useState([]);
@@ -17,11 +19,12 @@ const MatchingScreen = ({ navigation }) => {
 
         if (currentUserDoc.exists()) {
           const currentUserData = currentUserDoc.data();
-          if (currentUserData.matches && currentUserData.matches.length > 0) {
-            // Use the stored matches if they exist
-            setMatches(currentUserData.matches);
+          if (currentUserData.matches && Array.isArray(currentUserData.matches) && currentUserData.matches.length > 0) {
+            InteractionManager.runAfterInteractions(() => {
+              setMatches(currentUserData.matches);
+              setLoading(false);
+            });
           } else {
-            // Calculate matches if they don't exist
             const usersCollection = collection(firestore, 'users');
             const usersSnapshot = await getDocs(usersCollection);
 
@@ -30,7 +33,6 @@ const MatchingScreen = ({ navigation }) => {
             const filteredMatches = allUsers.filter(otherUser => {
               if (otherUser.id === user.uid) return false;
 
-              // Ensure all necessary fields are present
               const otherGender = otherUser.gender?.toLowerCase() || '';
               const otherSelectedGender = otherUser.selectedGender?.toLowerCase() || '';
               const otherActivity = otherUser.selectedActivity || '';
@@ -43,7 +45,6 @@ const MatchingScreen = ({ navigation }) => {
               const currentAge = currentUserData.selectedAge || '';
               const currentRegion = currentUserData.selectedRegion || '';
 
-              // Mutual matching criteria
               return (
                 otherGender === currentSelectedGender &&
                 currentGender === otherSelectedGender &&
@@ -53,21 +54,158 @@ const MatchingScreen = ({ navigation }) => {
               );
             });
 
-            // Store matches in Firestore
-            await setDoc(currentUserRef, { matches: filteredMatches }, { merge: true });
+            const matchesWithUsersAndChats = filteredMatches.map(match => ({
+              id: uuidv4(),
+              username: match.username,
+              users: [user.uid, match.id],
+              chats: []
+            }));
 
-            setMatches(filteredMatches);
+            // Update current user's matches
+            await updateDoc(currentUserRef, { matches: matchesWithUsersAndChats }, { merge: true });
+
+            // Update matches for other users
+            for (const match of matchesWithUsersAndChats) {
+              await updateMatchForOtherUser(match.users[1], {
+                id: match.id,
+                username: currentUserData.username || user.email,
+                users: [match.users[1], user.uid],
+                chats: []
+              });
+            }
+
+            InteractionManager.runAfterInteractions(() => {
+              setMatches(matchesWithUsersAndChats);
+              setLoading(false);
+            });
           }
         }
       } catch (error) {
         console.error('Error fetching matches:', error);
-      } finally {
+        Alert.alert('Error', `Error fetching matches: ${error.message}`);
         setLoading(false);
       }
     };
 
     fetchMatches();
-  }, []);
+  }, [user.uid]);
+
+  const updateMatchForOtherUser = async (otherUserId, matchData) => {
+    try {
+      const otherUserRef = doc(firestore, 'users', otherUserId);
+      const otherUserDoc = await getDoc(otherUserRef);
+
+      if (otherUserDoc.exists()) {
+        const otherUserData = otherUserDoc.data();
+        const existingMatches = otherUserData.matches || [];
+
+        // Check if the match already exists
+        const matchExists = existingMatches.some(match => match.id === matchData.id);
+
+        if (!matchExists) {
+          await updateDoc(otherUserRef, {
+            matches: arrayUnion(matchData)
+          });
+        }
+      }
+    } catch (error) {
+      console.error(`Error updating match for user ${otherUserId}:`, error);
+    }
+  };
+
+  const handleMatchClick = async (match) => {
+    const currentUserRef = doc(firestore, 'users', user.uid);
+    const matchUserRef = doc(firestore, 'users', match.id);
+
+    try {
+      const currentUserDoc = await getDoc(currentUserRef);
+      const matchUserDoc = await getDoc(matchUserRef);
+
+      if (!currentUserDoc.exists()) {
+        console.error(`Current user document does not exist: ${user.uid}`);
+        Alert.alert('Error', `Current user document does not exist: ${user.uid}`);
+        return;
+      }
+
+      if (!matchUserDoc.exists()) {
+        console.error(`Match user document does not exist: ${match.id}`);
+        Alert.alert('Error', `Match user document does not exist: ${match.id}`);
+        return;
+      }
+
+      const currentUserData = currentUserDoc.data();
+      const matchUserData = matchUserDoc.data();
+
+      // Initialize matches array if it doesn't exist
+      currentUserData.matches = currentUserData.matches || [];
+      matchUserData.matches = matchUserData.matches || [];
+
+      // Check if the match already exists for the current user
+      const matchExistsForCurrentUser = currentUserData.matches.some(
+        (m) => m.id === match.id
+      );
+
+      // Check if the match already exists for the matched user
+      const matchExistsForMatchUser = matchUserData.matches.some(
+        (m) => m.id === user.uid
+      );
+
+      if (matchExistsForCurrentUser && matchExistsForMatchUser) {
+        console.log('Match already exists for both users');
+        const existingMatch = currentUserData.matches.find(m => m.id === match.id);
+        navigation.navigate('Messages', { matchId: existingMatch.id, recipientId: match.id });
+        return;
+      }
+
+      const matchId = uuidv4();
+
+      const matchData = {
+        id: matchId,
+        username: match.username,
+        users: [user.uid, match.id],
+        chats: []
+      };
+
+      const reciprocalMatchData = {
+        id: matchId,
+        username: user.displayName || user.email,
+        users: [match.id, user.uid],
+        chats: []
+      };
+
+      console.log('Current User Ref:', currentUserRef);
+      console.log('Match User Ref:', matchUserRef);
+      console.log('Match Data:', matchData);
+      console.log('Reciprocal Match Data:', reciprocalMatchData);
+
+      // Update current user's matches if not already present
+      if (!matchExistsForCurrentUser) {
+        await updateDoc(currentUserRef, {
+          matches: arrayUnion(matchData)
+        });
+        console.log('Match added to current user:', user.uid);
+      }
+
+      // Update matched user's matches if not already present
+      if (!matchExistsForMatchUser) {
+        await updateDoc(matchUserRef, {
+          matches: arrayUnion(reciprocalMatchData)
+        });
+        console.log('Match added to matched user:', match.id);
+      }
+
+      navigation.navigate('Messages', { matchId, recipientId: match.id });
+    } catch (error) {
+      console.error('Error creating match:', error);
+      Alert.alert('Error', `Error creating match: ${error.message}`);
+    }
+  };
+
+  const renderMatch = ({ item }) => (
+    <TouchableOpacity style={styles.match} onPress={() => handleMatchClick(item)}>
+      <Text style={styles.matchText}>Username: {item.username || 'N/A'}</Text>
+    </TouchableOpacity>
+  );
 
   if (loading) {
     return (
@@ -78,32 +216,23 @@ const MatchingScreen = ({ navigation }) => {
   }
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.title}>Matches</Text>
-      {matches.length > 0 ? (
-        matches.map(match => (
-          <View key={match.id} style={styles.match}>
-            <Text style={styles.matchText}>Display Name: {match.displayName || 'N/A'}</Text>
-            <Text style={styles.matchText}>Username: {match.username || 'N/A'}</Text>
-            <Text style={styles.matchText}>Age: {match.age || 'N/A'}</Text>
-            <Text style={styles.matchText}>Gender: {match.gender || 'N/A'}</Text>
-            <Text style={styles.matchText}>Activity: {match.selectedActivity || 'N/A'}</Text>
-            <Text style={styles.matchText}>Region: {match.selectedRegion || 'N/A'}</Text>
-          </View>
-        ))
-      ) : (
-        <Text>No matches found.</Text>
-      )}
-    </ScrollView>
+      <FlatList
+        data={matches}
+        renderItem={renderMatch}
+        keyExtractor={item => item.id}
+        ListEmptyComponent={<Text>No matches found.</Text>}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
+    flex: 1,
     padding: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
   },
   title: {
     fontSize: 24,
@@ -111,7 +240,6 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   match: {
-    width: '100%',
     padding: 15,
     backgroundColor: '#f9f9f9',
     borderWidth: 1,
@@ -121,7 +249,6 @@ const styles = StyleSheet.create({
   },
   matchText: {
     fontSize: 16,
-    marginBottom: 5,
   },
 });
 
--- MessagingScreen.js
+++ MessagingScreen.js
@@ -1,46 +1,107 @@
-import React, {useEffect, useState } from 'react';
-import { View, FlatList, KeyboardAvoidingView, Platform, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, FlatList, KeyboardAvoidingView, Platform, StyleSheet, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import MessageInput from './MessageInput';
 import MessageBubble from './MessageBubble';
-import {auth, firestore} from './firebaseConfig'
-import { collection, addDoc, onSnapshot, query, orderBy, serverTimestamp } from 'firebase/firestore';
+import { auth, firestore } from './firebaseConfig';
+import { doc, getDoc, updateDoc, onSnapshot } from 'firebase/firestore';
+import { v4 as uuidv4 } from 'uuid';
+import 'react-native-get-random-values';
 
-//this is the screen that the user is routed to after clicking on a match. it's a chat with the matched user
 const MessagingScreen = ({ route }) => {
-  const { matchId } = route.params;
-  
+  const { matchId, recipientId } = route.params;
   const [messages, setMessages] = useState([]);
   const user = auth.currentUser;
 
   useEffect(() => {
-    if (!matchId) {
-      console.error('matchId is undefined');
+    if (!matchId || !user || !recipientId) {
+      console.error('Required parameters are undefined');
       return;
     }
-    const q = query(
-      collection(firestore, 'chats', matchId, 'messages'),
-      orderBy('timestamp', 'asc')
-    );
-    const unsubscribe = onSnapshot(q, snapshot => {
-      setMessages(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+
+    console.log("Current User:", user);
+    console.log("Recipient ID:", recipientId);
+
+    const userRef = doc(firestore, 'users', user.uid);
+
+    const unsubscribe = onSnapshot(userRef, (docSnapshot) => {
+      if (docSnapshot.exists()) {
+        const userData = docSnapshot.data();
+        const match = userData.matches ? userData.matches.find(match => match.id === matchId) : null;
+        if (match && match.chats) {
+          setMessages(match.chats);
+        }
+      }
+    }, (error) => {
+      console.error('Error listening to messages:', error);
     });
 
+    // Cleanup function
     return () => unsubscribe();
-  }, [matchId]);
-  
+  }, [matchId, user.uid, recipientId]);
+
   const handleSend = async (message) => {
+    if (!message.trim()) return;
+
+    if (!recipientId) {
+      console.error('Recipient ID is undefined');
+      Alert.alert('Error', 'Recipient ID is undefined');
+      return;
+    }
+
+    const newMessage = {
+      id: uuidv4(),
+      senderId: user.uid,
+      text: message,
+      timestamp: new Date().toISOString()
+    };
+
     try {
-      await addDoc(collection(firestore, 'chats', matchId, 'messages'), {
-        text: message,
-        senderId: user.uid, 
-        timestamp: serverTimestamp(),
-      });
+      const userRef = doc(firestore, 'users', user.uid);
+      const recipientRef = doc(firestore, 'users', recipientId);
+
+      const userDoc = await getDoc(userRef);
+      const recipientDoc = await getDoc(recipientRef);
+
+      if (userDoc.exists() && recipientDoc.exists()) {
+        const userData = userDoc.data();
+        const recipientData = recipientDoc.data();
+
+        const updateMatchMessages = (matches, matchId, newMessage) => {
+          if (!Array.isArray(matches)) matches = [];
+          return matches.map(match => {
+            if (match.id === matchId) {
+              return {
+                ...match,
+                chats: [...match.chats, newMessage]
+              };
+            }
+            return match;
+          });
+        };
+
+        const updatedUserMatches = updateMatchMessages(userData.matches, matchId, newMessage);
+        const updatedRecipientMatches = updateMatchMessages(recipientData.matches, matchId, newMessage);
+
+        await updateDoc(userRef, {
+          matches: updatedUserMatches
+        });
+
+        await updateDoc(recipientRef, {
+          matches: updatedRecipientMatches
+        });
+
+        // We don't need to manually update the messages state here
+        // as the onSnapshot listener will handle this for us
+      }
     } catch (error) {
-      console.error("Error adding document: ", error);
+      console.error("Error adding message: ", error);
+      Alert.alert("Error", `Error adding message: ${error.message}`);
     }
   };
+
+
   return (
-    <KeyboardAvoidingView 
+    <KeyboardAvoidingView
       style={styles.container}
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 0}
@@ -48,8 +109,8 @@ const MessagingScreen = ({ route }) => {
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.container}>
           <FlatList
-            data={messages} 
-            renderItem={({ item }) => <MessageBubble message={item.text} isOwnMessage={item.ownMessage} />}
+            data={messages}
+            renderItem={({ item }) => <MessageBubble message={item.text} isOwnMessage={item.senderId === user.uid} />}
             keyExtractor={(item) => item.id}
           />
           <MessageInput onSend={handleSend} autoFocus={true} />
