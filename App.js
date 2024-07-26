39adc199a06667106778961d5e2c224e23d8449a
amagee99 <107876195+amagee99@users.noreply.github.com>
2024-07-26 14:07:49 -0400

    swiping works

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import 'react-native-get-random-values';
 const Stack = createStackNavigator();
 
 export default function App() {
+  
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
